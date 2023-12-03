d99f48f61d2f5a7e4354ad1a8890e838fd1d7d78
khattab88 <m_khatab_88@hotmail.com>
2023-12-03 16:22:38 +0200

    adding events and tickets tables

--- FormulaOne.Data/AppDbContext.cs
+++ FormulaOne.Data/AppDbContext.cs
@@ -16,6 +16,8 @@ namespace FormulaOne.Data
 
         public virtual DbSet<Driver> Drivers { get; set; }
         public virtual DbSet<Achievement> Achievements { get; set; }
+        public virtual DbSet<Event> Events { get; set; }
+        public virtual DbSet<Ticket> Tickets { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -28,6 +30,42 @@ namespace FormulaOne.Data
                     .HasForeignKey(a => a.DriverId)
                     .OnDelete(DeleteBehavior.NoAction)
                     .HasConstraintName("FK_Achievements_Driver");
+            });
+
+            modelBuilder.Entity<Event>(entity =>
+            {
+                entity.HasMany<Ticket>(e => e.Tickets)
+                .WithOne()
+                .HasForeignKey(t => t.EventId)
+                .IsRequired();
+
+                var demoEvent = new Event()
+                {
+                    Id = 1,
+                    Location = "Silverstone",
+                    Name = "British Brexit"
+                };
+
+                entity.HasData(demoEvent);
+            });
+
+            modelBuilder.Entity<Ticket>(entity => 
+            {
+                var tickets = Enumerable
+                    .Range(0, 5000)
+                    .Select(id => new Ticket() 
+                    {
+                        Id = Guid.NewGuid(),
+                        EventDate = DateTime.UtcNow.AddDays(10),
+                        Price = 100,
+                        TicketLevel = "Bronze",
+                        EventId = 1,
+                        Status = 1,
+                        CreatedAt = DateTime.UtcNow,
+                        UpdatedAt = DateTime.UtcNow,
+                    });
+
+                entity.HasData(tickets);
             });
         }
     }
---
+++ FormulaOne.Entities/Ticket.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FormulaOne.Entities
+{
+    public class Ticket : BaseEntity
+    {
+        public int EventId { get; set; }
+        public DateTime EventDate { get; set; }
+        public double Price { get; set; }
+        public string TicketLevel { get; set; } = string.Empty;
+    }
+}
