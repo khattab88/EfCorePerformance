e076cfce02c448b30c09c373d11ae95045c81edd
khattab88 <m_khatab_88@hotmail.com>
2023-12-03 18:33:34 +0200

    create tickets controller

---
+++ FormulaOne.API/Controllers/TicketsController.cs
@@ -0,0 +1,55 @@
+﻿using FormulaOne.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FormulaOne.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public TicketsController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTickets()
+        {
+            var tickets = await _db.Events
+                .Include(e => e.Tickets)
+                .ToListAsync();
+
+            return Ok(tickets);
+        }
+
+        [HttpPut]
+        [Route("UpdateTicketPrices")]
+        public async Task<IActionResult> UpdateTicketPrices(int eventId, double amount)
+        {
+            var mainEvent = await _db.Events
+                .Include(e => e.Tickets)
+                .FirstOrDefaultAsync(e => e.Id == eventId);
+
+            if (mainEvent == null)
+            {
+                return NotFound();
+            }
+
+            foreach (var ticket in mainEvent.Tickets)
+            {
+                ticket.Price *= amount;
+                ticket.UpdatedAt = DateTime.UtcNow;
+            }
+
+            mainEvent.Location = mainEvent.Location + " - updated at " + DateTime.UtcNow.Millisecond;
+
+            await _db.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
